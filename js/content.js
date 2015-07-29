6acd0c8185c344ee7e5c3332d64be40756783880
大石 未央 <mio.oishi@linkazia.com>
2015-07-29 22:40:21 +0900

    タスクの入れ替え処理

--- js/content.js
+++ js/content.js
@@ -3,12 +3,15 @@ $(window).on('load resize', function(){
     jQuery('.file_slide').bxSlider({
       mode: 'fade',
       adaptiveHeight: 'true'
-      
+
     });
     $(".bookmark").click(
       function(){
         $(".bookmark").toggleClass("active");
       }
     );
+  });
+  $(function(){
+  	$('.person_charge').sortable();
   });
 });
