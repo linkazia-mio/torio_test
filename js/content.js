f605860745c323dcfdb965ed6157eb49d75e023a
大石未央 <mio.oishi@linkazia.com>
2015-07-30 19:59:09 +0900

    タスク入れ変えの調整中

--- js/content.js
+++ js/content.js
@@ -10,34 +10,6 @@ $(window).on('load resize', function(){
         $('.bookmark').toggleClass('active');
       }
     );
-    $('#nestable').nestable({
-    });
-    $('#task_1').click(function() {
-      $('#task_1').css( 'display', 'none');
-      $('#task_edit_1')
-          .val( $( '#task_1').text())
-          .css( 'display', '')
-          .focus();
-    });
-    $('#task_edit_1').blur(function() {
-        $('#task_edit_1').css( 'display', 'none');
-        $('#task_1')
-          .text($('#task_edit_1').val())
-          .css( 'display', '');
-    });
-    $('#task_2').click(function() {
-      $('#task_2').css( 'display', 'none');
-      $('#task_edit_2')
-          .val( $( '#task_2').text())
-          .css( 'display', '')
-          .focus();
-    });
-    $('#task_edit_2').blur(function() {
-        $('#task_edit_2').css( 'display', 'none');
-        $('#task_2')
-          .text($('#task_edit_2').val())
-          .css( 'display', '');
-    });
     $('.favorite').click(
       function(){
         $('.favorite').toggleClass('active');
