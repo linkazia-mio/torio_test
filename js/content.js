62e309a8969fbf2456553bb13ce121a017c29855
大石未央 <mio.oishi@linkazia.com>
2015-07-30 17:39:24 +0900

    動かないタスクも作成

--- js/content.js
+++ js/content.js
@@ -11,7 +11,19 @@ $(window).on('load resize', function(){
       }
     );
     $('#nestable').nestable({
-      maxdepth: 2
     });
+    $('#task_1').click(function() {
+      $('#task_1').css( 'display', 'none');
+      $('#task_edit_1')
+          .val( $( '#task_1').text())
+          .css( 'display', '')
+          .focus();
+  });
+  $('#task_edit_1').blur(function() {
+      $('#task_edit_1').css( 'display', 'none');
+      $('#task_1')
+        .text($('#task_edit_1').val())
+        .css( 'display', '');
+      });
   });
 });
