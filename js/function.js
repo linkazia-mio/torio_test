39fd0e33c0ba498f2d0026b18cdb0eaced3f4887
大石未央 <mio.oishi@linkazia.com>
2015-07-31 16:11:44 +0900

    jsがおかしくなってたので一つにまとめてみた

--- js/function.js
+++ js/function.js
@@ -252,4 +252,104 @@ window.onload=function(){
   var myDoughnut = new Chart(document.getElementById("task_graph_yellow").
   getContext("2d")).Doughnut(Dougnut_data_yellow,options);
 
+
+  jQuery(document).ready(function(){
+    var slider = jQuery('.file_slide').bxSlider({
+      mode: 'fade',
+      adaptiveHeight: 'true'
+    });
+
+    jQuery('#file_detail').on('show.bs.modal', function(){
+        setTimeout(function(){
+                slider.reloadSlider();}, 200
+        )
+    })
+
+    $('.bookmark').click(
+      function(){
+        $('.bookmark').toggleClass('active');
+      }
+    );
+    $('.favorite').click(
+      function(){
+        $('.favorite').toggleClass('active');
+      }
+    );
+    jQuery('.task_replacement').sortable({
+      cursor : 'move',
+      handle : '.handle'
+    });
+    jQuery('.task_lists').sortable({
+      cursor : 'move',
+      connectWith : '.task_lists'
+    });
+  });
+
+  
+  var editor = new MediumEditor('.editable', {
+      buttonLabels: 'fontawesome',
+      buttons: [
+          'bold',
+      //    'italic',
+      //    'underline',
+          'header1',
+          'anchor',
+      //    'header2',
+          'quote',
+      //    'superscript',
+      //    'subscript',
+      //    'strikethrough',
+          'unorderedlist',
+          'orderedlist',
+          'pre',
+      //    'justifyLeft',
+      //    'justifyFull',
+      //    'justifyCenter',
+      //    'justifyRight',
+      //    'image',
+      //    'indent',
+      //    'outdent',
+      //    'removeFormat'
+      ],
+      anchorInputPlaceholder: 'Type a link',
+      //diffLeft: 25,
+      //diffTop: 10,
+      firstHeader: 'h1',
+      secondHeader: 'h2',
+      delay: 1000,
+      targetBlank: true,
+      paste: {
+          cleanPastedHTML: true,
+          cleanAttrs: ['style', 'dir'],
+          cleanTags: ['label', 'meta']
+      }
+  });
+  $(function () {
+    $('.editable').mediumInsert({
+      editor: editor,
+      addons: {
+        images: {
+          uploadScript: null,
+          deleteScript: null,
+          styles: {
+            slideshow: {
+              label: '<span class="fa fa-play"></span>',
+              added: function ($el) {
+                $el
+                .data('cycle-center-vert', true)
+                .cycle({
+                    slides: 'figure'
+                });
+              },
+              removed: function ($el) {
+                  $el.cycle('destroy');
+              }
+            }
+          },
+          actions: null
+        }
+      }
+    });
+  });
+
 };
