c2d15ba27ac08b3c424edf8191b639950fe1dbf2
大石未央 <mio.oishi@linkazia.com>
2015-07-31 15:48:46 +0900

    インデントを揃えた

--- js/content.js
+++ js/content.js
@@ -76,19 +76,19 @@ $(window).on('load resize', function(){
           uploadScript: null,
           deleteScript: null,
           styles: {
-              slideshow: {
-                  label: '<span class="fa fa-play"></span>',
-                  added: function ($el) {
-                      $el
-                      .data('cycle-center-vert', true)
-                      .cycle({
-                          slides: 'figure'
-                      });
-                  },
-                  removed: function ($el) {
-                      $el.cycle('destroy');
-                  }
+            slideshow: {
+              label: '<span class="fa fa-play"></span>',
+              added: function ($el) {
+                  $el
+                  .data('cycle-center-vert', true)
+                  .cycle({
+                      slides: 'figure'
+                  });
+              },
+              removed: function ($el) {
+                  $el.cycle('destroy');
               }
+            }
           },
           actions: null
         }
