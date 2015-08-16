859b7115d2871b2a7de9465624376761d574918e
大石 未央 <mio.oishi@linkazia.com>
2015-08-16 20:48:24 +0900

    jsを整理

--- js/function.js
+++ js/function.js
@@ -3,291 +3,18 @@ $(window).on('resize', function(){});
 window.onload=function(){
 
   $(document).ready(function(){
-    //カレンダー
-    $(function() {
-        $('#made_1 .calendar').datepicker({
-    		showOtherMonths: true,
-    		minDate: 0,
-    		onSelect: function(dateText, inst) {
-    			$('#made_1 #date').val(dateText);
-    		}
-        });
-    });
-    $(function() {
-        $('#made_2 .calendar').datepicker({
-    		showOtherMonths: true,
-    		minDate: 0,
-    		onSelect: function(dateText, inst) {
-    			$('#made_2 #date').val(dateText);
-    		}
-        });
-    });
-    $(function() {
-        $('#made_3 .calendar').datepicker({
-    		showOtherMonths: true,
-    		minDate: 0,
-    		onSelect: function(dateText, inst) {
-    			$('#made_3 #date').val(dateText);
-    		}
-        });
-    });
-    $(function() {
-        $('.limit_add_time .calendar').datepicker({
-    		showOtherMonths: true,
-    		minDate: 0,
-    		onSelect: function(dateText, inst) {
-    			$('#limlt_date').val(dateText);
-    		}
-        });
+
+    //global_menu 非表示
+    $('.global_menu_btn').click(function () {
+      jQuery('.global_menu').toggle();
+      jQuery('.global_menu').toggleClass("size_none");
+      jQuery('#masonry').masonry();
+      var w = $(window).width(); //ウィンドウの高さ
+      var w1= $('.page_menu').width(); //他要素の高さ
+      var w2= $('.global_menu').width(); //他要素の高さ
+      $('.detail').css('width', w-w1-w2-2); //可変部分の高さを適用
     });
 
-
-    (function( factory ) {
-      	if ( typeof define === 'function' && define.amd ) {
-      		// AMD. Register as an anonymous module.
-      		define([ '../datepicker' ], factory );
-      	} else {
-      		// Browser globals
-      		factory( jQuery.datepicker );
-      	}
-      }(function( datepicker ) {
-      datepicker.regional['ja'] = {
-      	closeText: '閉じる',
-      	prevText: '',
-      	nextText: '',
-      	currentText: '今日',
-      	monthNames: ['1月','2月','3月','4月','5月','6月',
-      	'7月','8月','9月','10月','11月','12月'],
-      	monthNamesShort: ['1月','2月','3月','4月','5月','6月',
-      	'7月','8月','9月','10月','11月','12月'],
-      	dayNames: ['日曜日','月曜日','火曜日','水曜日','木曜日','金曜日','土曜日'],
-      	dayNamesShort: ['日','月','火','水','木','金','土'],
-      	dayNamesMin: ['日','月','火','水','木','金','土'],
-      	weekHeader: '週',
-      	dateFormat: 'yy/mm/dd',
-      	firstDay: 0,
-      	isRTL: false,
-      	showMonthAfterYear: true,
-      	yearSuffix: '年'};
-        datepicker.setDefaults(datepicker.regional['ja']);
-        return datepicker.regional['ja'];
-      }));
-
-      //ノートタイトル書き換え
-      $('.note_title').click(function() {
-        $('.note_title').css( 'display', 'none');
-        $('#title_edit')
-          .val( $( '.note_title').text())
-          .css( 'display', '')
-          .focus();
-      });
-        $('#title_edit').blur(function() {
-        $('#title_edit').css( 'display', 'none');
-        $('.note_title')
-        .text($('#title_edit').val())
-        .css( 'display', '');
-      });
-
-      $('.editable').click(function () {
-        jQuery('.start').hide();
-        event.stopPropagation();
-      });
-
-  });
-
-  //グループを追加の削除ボタン
-  $('#group_member_1 .delete').click(function() {
-    $('#group_member_1').hide();
-  });
-  $('#group_member_2 .delete').click(function() {
-    $('#group_member_2').hide();
-  });
-
-  //トグルスイッチ
-  $('.toggle_btn').click(function () {
-    $('.off .circle').animate({'left': '+=30px'}, 'fast');
-    $('.on .circle').animate({'left': '-=30px'}, 'fast');
-    $(this).toggleClass("off");
-    $(this).toggleClass("on");
-    $('.input_dot_contents p .on').toggle();
-    $('.input_dot_contents p .off').toggle();
-  });
-
-  //グループ、メンバー追加hover
-  $('.add').click(function() {
-    $(this).find('.balloon_box').hide();
-  });
-  $('.add').hover(
-    function () {
-      $(this).find('.balloon_box').show();
-    },
-    function () {
-      $(this).find('.balloon_box').hide();
-    }
-  );
-
-  //閲覧者hover
-  $('.browse_count').click(function() {
-    $(this).find('.balloon_box').hide();
-  });
-  $('.browse_count').hover(
-    function () {
-      $(this).find('.balloon_box').show();
-    },
-    function () {
-      $(this).find('.balloon_box').hide();
-    }
-  );
-
-  //クリッカブル
-  $('.item').click(function () {
-    $(this).toggleClass('selected');
-  });
-  //クリッカブル
-  $('.global_menu_inner li a').click(function () {
-    $(this).toggleClass('selected');
-  });
-
-  //リストを作成するボタン表示
-  $('.task_create_inner input').click(function () {
-    jQuery('.btn_add_task_create').show();
-    event.stopPropagation();
-  });
-  $(document).click(function() {
-    $('.btn_add_task_create').hide();
-  });
-
-  //タスク入力部分
-  $('.task_add_list_member button').click(function () {
-    $(this).toggleClass('checked');
-  });
-  $('.task_add_member button').click(function () {
-    $(this).toggleClass('checked');
-  });
-  $('.task_add_requester button').click(function () {
-    $(this).toggleClass('checked');
-  });
-  $('.task_add_worker button').click(function () {
-    $(this).toggleClass('checked');
-  });
-
-
-  $('#made_1.made .task_input').click(function () {
-    jQuery('#made_1 .task_button').show();
-    event.stopPropagation();
-  });
-  $(document).click(function() {
-    $('#made_1 .task_button').hide();
-  });
-
-  $('#made_2.made').click(function () {
-    jQuery('#made_2 .task_button').show();
-    event.stopPropagation();
-  });
-  $(document).click(function() {
-    $('#made_2 .task_button').hide();
-  });
-
-  $('#made_3.made').click(function () {
-    jQuery('#made_3 .task_button').show();
-    event.stopPropagation();
-  });
-  $(document).click(function() {
-    $('#made_3 .task_button').hide();
-  });
-
-
-  $('.information textarea').each(function(){
-    autosize(this);
-  });
-
-  $(function(){
-       function adjust(){
-            var h = $(window).height(); //ウィンドウの高さ
-            $('.main').css('height', h); //可変部分の高さを適用
-       };
-       function adjust(){
-            var h = $(window).height(); //ウィンドウの高さ
-            var h1= $('.page_menu .config').height(); //他要素の高さ
-            var h2= $('.page_menu .page_menu_footer').height(); //他要素の高さ
-            $('.page_menu_inner').css('height', h-h1-h2-53); //可変部分の高さを適用
-       }
-       adjust();
-       $(window).on('resize', function(){
-            adjust();
-       })
-  });
-
-  $(function(){
-        function adjust(){
-             var h = $(window).height(); //ウィンドウの高さ
-             var h4= $('.profile_title').height(); //他要素の高さ
-             $('.profile').css('height', h-h4-53); //可変部分の高さを適用
-        }
-       adjust();
-       $(window).on('resize', function(){
-            adjust();
-       })
-  });
-
-  $(function(){
-        function adjust(){
-             var h = $(window).height(); //ウィンドウの高さ
-             $('.global_menu_inner').css('height', h-53); //可変部分の高さを適用
-        }
-       adjust();
-       $(window).on('resize', function(){
-            adjust();
-       })
-  });
-
-  $(function(){
-        function adjust(){
-             var h = $(window).height(); //ウィンドウの高さ
-             $('.detail').css('height', h-50); //可変部分の高さを適用
-        }
-       adjust();
-       $(window).on('resize', function(){
-            adjust();
-       })
-  });
-
-  $(function(){
-      function adjust(){
-           var w = $(window).width(); //ウィンドウの高さ
-           var w1= $('.page_menu').width(); //他要素の高さ
-           var w2= $('.global_menu').width(); //他要素の高さ
-           $('.detail').css('width', w-w1-w2-2); //可変部分の高さを適用
-      }
-     adjust();
-     $(window).on('resize', function(){
-        adjust();
-     })
-  });
-
-
-  $(function(){
-       function adjust(){
-            var h = $(window).height(); //ウィンドウの高さ
-            $('.viewers_list').css('max-height', h/2); //可変部分の高さを適用
-       }
-       adjust();
-       $(window).on('resize', function(){
-            adjust();
-       })
-  });
-
-
-  var popover = $('.popover');
-  var popoverBig = $('.popover_big');
-  //エリア外をクリックしたらpopoverを消す
-  $(document).click(function() {
-    popover.hide();
-    popoverBig.hide();
-  });
-
-
-  $(document).ready(function(){
     //Task Box
     $('.alart_task p').click(function () {
       $('.task_box').toggle( 'fade', '', 100 );
@@ -552,7 +279,325 @@ window.onload=function(){
       event.stopPropagation();
     });
 
+    //カレンダー
+    $(function() {
+        $('#made_1 .calendar').datepicker({
+    		showOtherMonths: true,
+    		minDate: 0,
+    		onSelect: function(dateText, inst) {
+    			$('#made_1 #date').val(dateText);
+    		}
+        });
+    });
+    $(function() {
+        $('#made_2 .calendar').datepicker({
+    		showOtherMonths: true,
+    		minDate: 0,
+    		onSelect: function(dateText, inst) {
+    			$('#made_2 #date').val(dateText);
+    		}
+        });
+    });
+    $(function() {
+        $('#made_3 .calendar').datepicker({
+    		showOtherMonths: true,
+    		minDate: 0,
+    		onSelect: function(dateText, inst) {
+    			$('#made_3 #date').val(dateText);
+    		}
+        });
+    });
+    $(function() {
+        $('.limit_add_time .calendar').datepicker({
+    		showOtherMonths: true,
+    		minDate: 0,
+    		onSelect: function(dateText, inst) {
+    			$('#limlt_date').val(dateText);
+    		}
+        });
+    });
 
+
+    (function( factory ) {
+      	if ( typeof define === 'function' && define.amd ) {
+      		// AMD. Register as an anonymous module.
+      		define([ '../datepicker' ], factory );
+      	} else {
+      		// Browser globals
+      		factory( jQuery.datepicker );
+      	}
+      }(function( datepicker ) {
+      datepicker.regional['ja'] = {
+      	closeText: '閉じる',
+      	prevText: '',
+      	nextText: '',
+      	currentText: '今日',
+      	monthNames: ['1月','2月','3月','4月','5月','6月',
+      	'7月','8月','9月','10月','11月','12月'],
+      	monthNamesShort: ['1月','2月','3月','4月','5月','6月',
+      	'7月','8月','9月','10月','11月','12月'],
+      	dayNames: ['日曜日','月曜日','火曜日','水曜日','木曜日','金曜日','土曜日'],
+      	dayNamesShort: ['日','月','火','水','木','金','土'],
+      	dayNamesMin: ['日','月','火','水','木','金','土'],
+      	weekHeader: '週',
+      	dateFormat: 'yy/mm/dd',
+      	firstDay: 0,
+      	isRTL: false,
+      	showMonthAfterYear: true,
+      	yearSuffix: '年'};
+        datepicker.setDefaults(datepicker.regional['ja']);
+        return datepicker.regional['ja'];
+      }));
+
+      //ノートタイトル書き換え
+      $('.note_title').click(function() {
+        $('.note_title').css( 'display', 'none');
+        $('#title_edit')
+          .val( $( '.note_title').text())
+          .css( 'display', '')
+          .focus();
+      });
+        $('#title_edit').blur(function() {
+        $('#title_edit').css( 'display', 'none');
+        $('.note_title')
+        .text($('#title_edit').val())
+        .css( 'display', '');
+      });
+
+      $('.editable').click(function () {
+        jQuery('.start').hide();
+        event.stopPropagation();
+      });
+
+      //ファイルスライド
+      
+      var slider = jQuery('.file_slide').bxSlider({
+        mode: 'fade',
+        adaptiveHeight : true,
+        preloadImages: 'visible',
+        autoReload: true
+      });
+
+
+      jQuery('#file_detail').on('show.bs.modal', function(){
+        setTimeout(function(){slider.reloadSlider();},200);
+      });
+
+      //タスク入れ替え
+      jQuery('.task_replacement').sortable({
+        cursor : 'move',
+        handle : '.handle',
+        placeholder: 'placeholder'
+      });
+      jQuery('.task_lists').sortable({
+        cursor : 'move',
+        connectWith : '.task_lists',
+        placeholder: 'placeholder_inner'
+      });
+
+      $('.bookmark').click(
+        function(){
+          $('.bookmark').toggleClass('active');
+        }
+      );
+      $('.favorite').click(
+        function(){
+          $('.favorite').toggleClass('active');
+        }
+      );
+
+
+  });
+
+  //グループを追加の削除ボタン
+  $('#group_member_1 .delete').click(function() {
+    $('#group_member_1').hide();
+  });
+  $('#group_member_2 .delete').click(function() {
+    $('#group_member_2').hide();
+  });
+
+  //トグルスイッチ
+  $('.toggle_btn').click(function () {
+    $('.off .circle').animate({'left': '+=30px'}, 'fast');
+    $('.on .circle').animate({'left': '-=30px'}, 'fast');
+    $(this).toggleClass("off");
+    $(this).toggleClass("on");
+    $('.input_dot_contents p .on').toggle();
+    $('.input_dot_contents p .off').toggle();
+  });
+
+  //グループ、メンバー追加hover
+  $('.add').click(function() {
+    $(this).find('.balloon_box').hide();
+  });
+  $('.add').hover(
+    function () {
+      $(this).find('.balloon_box').show();
+    },
+    function () {
+      $(this).find('.balloon_box').hide();
+    }
+  );
+
+  //閲覧者hover
+  $('.browse_count').click(function() {
+    $(this).find('.balloon_box').hide();
+  });
+  $('.browse_count').hover(
+    function () {
+      $(this).find('.balloon_box').show();
+    },
+    function () {
+      $(this).find('.balloon_box').hide();
+    }
+  );
+
+  //クリッカブル
+  $('.item').click(function () {
+    $(this).toggleClass('selected');
+  });
+  //クリッカブル
+  $('.global_menu_inner li a').click(function () {
+    $(this).toggleClass('selected');
+  });
+
+  //リストを作成するボタン表示
+  $('.task_create_inner input').click(function () {
+    jQuery('.btn_add_task_create').show();
+    event.stopPropagation();
+  });
+  $(document).click(function() {
+    $('.btn_add_task_create').hide();
+  });
+
+  //タスク入力部分
+  $('.task_add_list_member button').click(function () {
+    $(this).toggleClass('checked');
+  });
+  $('.task_add_member button').click(function () {
+    $(this).toggleClass('checked');
+  });
+  $('.task_add_requester button').click(function () {
+    $(this).toggleClass('checked');
+  });
+  $('.task_add_worker button').click(function () {
+    $(this).toggleClass('checked');
+  });
+
+
+  $('#made_1.made .task_input').click(function () {
+    jQuery('#made_1 .task_button').show();
+    event.stopPropagation();
+  });
+  $(document).click(function() {
+    $('#made_1 .task_button').hide();
+  });
+
+  $('#made_2.made').click(function () {
+    jQuery('#made_2 .task_button').show();
+    event.stopPropagation();
+  });
+  $(document).click(function() {
+    $('#made_2 .task_button').hide();
+  });
+
+  $('#made_3.made').click(function () {
+    jQuery('#made_3 .task_button').show();
+    event.stopPropagation();
+  });
+  $(document).click(function() {
+    $('#made_3 .task_button').hide();
+  });
+
+
+  $('.information textarea').each(function(){
+    autosize(this);
+  });
+
+  $(function(){
+       function adjust(){
+            var h = $(window).height(); //ウィンドウの高さ
+            $('.main').css('height', h); //可変部分の高さを適用
+       };
+       function adjust(){
+            var h = $(window).height(); //ウィンドウの高さ
+            var h1= $('.page_menu .config').height(); //他要素の高さ
+            var h2= $('.page_menu .page_menu_footer').height(); //他要素の高さ
+            $('.page_menu_inner').css('height', h-h1-h2-53); //可変部分の高さを適用
+       }
+       adjust();
+       $(window).on('resize', function(){
+            adjust();
+       })
+  });
+
+  $(function(){
+        function adjust(){
+             var h = $(window).height(); //ウィンドウの高さ
+             var h4= $('.profile_title').height(); //他要素の高さ
+             $('.profile').css('height', h-h4-53); //可変部分の高さを適用
+        }
+       adjust();
+       $(window).on('resize', function(){
+            adjust();
+       })
+  });
+
+  $(function(){
+        function adjust(){
+             var h = $(window).height(); //ウィンドウの高さ
+             $('.global_menu_inner').css('height', h-53); //可変部分の高さを適用
+        }
+       adjust();
+       $(window).on('resize', function(){
+            adjust();
+       })
+  });
+
+  $(function(){
+        function adjust(){
+             var h = $(window).height(); //ウィンドウの高さ
+             $('.detail').css('height', h-50); //可変部分の高さを適用
+        }
+       adjust();
+       $(window).on('resize', function(){
+            adjust();
+       })
+  });
+
+  $(function(){
+      function adjust(){
+           var w = $(window).width(); //ウィンドウの高さ
+           var w1= $('.page_menu').width(); //他要素の高さ
+           var w2= $('.global_menu').width(); //他要素の高さ
+           $('.detail').css('width', w-w1-w2-2); //可変部分の高さを適用
+      }
+     adjust();
+     $(window).on('resize', function(){
+        adjust();
+     })
+  });
+
+
+  $(function(){
+       function adjust(){
+            var h = $(window).height(); //ウィンドウの高さ
+            $('.viewers_list').css('max-height', h/2); //可変部分の高さを適用
+       }
+       adjust();
+       $(window).on('resize', function(){
+            adjust();
+       })
+  });
+
+
+  var popover = $('.popover');
+  var popoverBig = $('.popover_big');
+  //エリア外をクリックしたらpopoverを消す
+  $(document).click(function() {
+    popover.hide();
+    popoverBig.hide();
   });
 
   $(function(){
@@ -561,19 +606,6 @@ window.onload=function(){
     });
   });
 
-
-  //global_menu 非表示
-  $(document).ready(function(){
-    $('.global_menu_btn').click(function () {
-      jQuery('.global_menu').toggle();
-      jQuery('.global_menu').toggleClass("size_none");
-      jQuery('#masonry').masonry();
-      var w = $(window).width(); //ウィンドウの高さ
-      var w1= $('.page_menu').width(); //他要素の高さ
-      var w2= $('.global_menu').width(); //他要素の高さ
-      $('.detail').css('width', w-w1-w2-2); //可変部分の高さを適用
-    });
-  });
 
   //画面幅移動
   jQuery(function() {
@@ -661,40 +693,6 @@ window.onload=function(){
   var myDoughnut = new Chart(document.getElementById('task_graph_yellow').
   getContext('2d')).Doughnut(Dougnut_data_yellow,options);
 
-  jQuery(document).ready(function(){
-    var slider = jQuery('.file_slide').bxSlider({
-      mode: 'fade',
-      adaptiveHeight : true,
-    });
-
-
-    jQuery('#file_detail').on('show.bs.modal', function(){
-        setTimeout(function(){slider.reloadSlider();},200);
-    });
-
-    //タスク入れ替え
-    jQuery('.task_replacement').sortable({
-      cursor : 'move',
-      handle : '.handle',
-      placeholder: 'placeholder'
-    });
-    jQuery('.task_lists').sortable({
-      cursor : 'move',
-      connectWith : '.task_lists',
-      placeholder: 'placeholder_inner'
-    });
-
-    $('.bookmark').click(
-      function(){
-        $('.bookmark').toggleClass('active');
-      }
-    );
-    $('.favorite').click(
-      function(){
-        $('.favorite').toggleClass('active');
-      }
-    );
-  });
 };
 
 $(function(){
